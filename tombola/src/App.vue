4d6f1dd95a099cb72e57528c49efd8aa681641ba
Héctor Sevilla <h2d_07@hotmail.com>
2018-11-28 23:25:20 -0600

    tombola completada

--- tombola/src/App.vue
+++ tombola/src/App.vue
@@ -1,8 +1,21 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <h1>Attendees: {{ attendees }}</h1>
-    <h2>The winner is: <strong> {{ winner }} </strong></h2>
+    <img src="./assets/iniser.png" id="logo">
+
+
+    <h2 v-if="winner" class="winnerTitle">Y el ganador es</h2>
+    <div class="premios">
+      <img src="./assets/premios.png" alt="">
+      <h2><strong> {{ winner }} </strong></h2>
+    </div>
+
+    <img v-if="winner" src="./assets/ganador.png" class="ganadorImagen" alt="">
+
+    <button  v-on:click="getwinner">Obtener Ganador!</button>
+
+
+
+
   </div>
 </template>
 
@@ -12,48 +25,108 @@ import axios from 'axios';
 import pickRandom from 'pick-random';
 import firebase from 'firebase';
 
+let config = {
+  //TU CONFIGURACIÓN
+    apiKey: "AIzaSyBlk4XI73nkLAZtwLmnNo5aLwp1OAJivXE",
+    authDomain: "tombola-c9919.firebaseapp.com",
+    databaseURL: "https://tombola-c9919.firebaseio.com",
+    projectId: "tombola-c9919",
+    storageBucket: "tombola-c9919.appspot.com",
+    messagingSenderId: "98129464472"
+}
+let app = firebase.initializeApp(config);
+let db = app.database();
+let fbattendees = db.ref();
+
 export default {
   name: 'app',
+
+  firebase: {
+    fbattendees: fbattendees
+  },
   mounted() {
-    this.getWinner();
+
   },
   data () {
     return {
       msg: 'Raffle with Vue.js',
-      attendees: [
-        'Hector',
-        'Rikkert',
-        'Jeykel',
-        'Francisco',
-        'Dratzen'
-      ],
-    winner: null
+      fbattendees: null,
+      winner: null
     }
   },
   methods: {
-    getAttendees(){
-      /*axios.get(URL)
-        .then( (response) => {
-          this.attendees = response.data.attendees;
-        });*/
-      
 
-    },// END GET ATTENDEES
-    getWinner(){
-      this.winner = pickRandom(this.attendees, {count:1});
+    getwinner: function(){
+
+
+      this.winner = pickRandom(this.fbattendees, {count:1});
+      this.winner = this.winner[0]['Nombre'] + ' ' + this.winner[0]['Apellidos'];
     }
   }
+
+
 }
 </script>
 
 <style>
+body,
+h1,
+h2 {
+  margin: 0;
+}
+.ganadorImagen{
+  display: block;
+  margin-left: auto;
+  margin: auto;
+}
+.winnerTitle{
+  color: white;
+  font-weight: 700;
+  font-size: 40px;
+}
+body{
+  position: relative;
+  background-color: blue;
+}
+.premios{
+  position: relative;
+}
+.premios h2{
+  position: absolute;
+  bottom: 52px;
+text-align: center;
+left: 50%;
+transform: translateX(-50%);
+font-size: 26px;
+}
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin: 0;
+  padding-top: 12rem;
+  padding-bottom: 2rem;
+}
+button{
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  background-color: #f5a604;
+  color: white;
+  font-size: 22px;
+    border-radius: 4px;
+    font-weight: 700;
+    border-right: 6px solid #ec8005;
+border-bottom: 6px solid #ec8005;
+border-top: 0;
+border-left: 0;
+padding: .5rem 1rem;
+}
+#logo{
+  position: absolute;
+  top: 0;
+  right: 10%;
+  max-width: 20%;
 }
 
 h1, h2 {
--- tombola/src/main.js
+++ tombola/src/main.js
@@ -1,5 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueFire from 'vuefire'
+
+Vue.use(VueFire)
 
 new Vue({
   el: '#app',
